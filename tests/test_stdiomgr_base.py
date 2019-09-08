5be43e38d0329be6119b09040b0939bfd4bf97d8
John Vandenberg <jayvdb@gmail.com>
2019-09-08 19:57:56 +0700

    All

--- tests/test_stdiomgr_base.py
+++ tests/test_stdiomgr_base.py
@@ -52,9 +52,6 @@ def test_context_manager_instantiation():
     """Confirm StdioManager instance is a tuple and registered context manager."""
     cm = StdioManager()
 
-    assert sys.stdout.isatty()
-    assert is_stdout_buffered()
-
     assert isinstance(cm, tuple)
 
     value_list = list(cm)
